db8733e9c0407ce1d3f1d9a52cf5181bc38b6dc4
João Pedro Barroso da Silva Neto <21802173@aluno.cotemig.com.br>
2023-05-07 18:26:19 -0300

    Update station.dart

--- bin/model/station.dart
+++ bin/model/station.dart
@@ -13,11 +13,16 @@ class Station {
   Instruction? currentInstruction;
   List<Instruction>? waitingInstruction;
 
-  void loadInstruction(
-      {required int id,
-      required Instruction instruction,
-      required Map<OpCode, int> costs,
-      required Map<int, Tupla> registers}) {
+  //colocar ocupado nos registradores // OK
+  //terminar o loadInstruction pra colocar na station
+  //fazer as dempendecias verdadeiras e falsas funcinarem pelos registradores
+  //rodar cada instrução
+  void loadInstruction({
+    required int id,
+    required Instruction instruction,
+    required Map<OpCode, int> costs,
+    required Map<int, Tupla> registers,
+  }) {
     print('loading instruction');
 
     instruction.id = id;
