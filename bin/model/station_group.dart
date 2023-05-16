ba95b4f253934fc4db7702f6d0232748b516c092
João Pedro Barroso da Silva Neto <21802173@aluno.cotemig.com.br>
2023-05-15 21:13:37 -0300

    Update station_group.dart

--- bin/model/station_group.dart
+++ bin/model/station_group.dart
@@ -49,6 +49,13 @@ class StationGroup {
       } else {
         if (instrucoes.length != 0) {
           for (int i = 0; i < instrucoes.length; i++) {
+            int posicao = perquisaInstrucao(
+                reOrderBuffer: reOrderBuffer, instruction: instrucoes[i]);
+
+            for (int j = 0; j < posicao; j++) {
+              if (!dependenciaVerdadeira(
+                  reOrderBuffer[j], instrucoes[i], element)) {}
+            }
             if (verifyStateRegisters(registers, instrucoes[i]) == false) {
               loadInstruction(instruction: instrucoes[i], registers: registers);
               instrucoes.remove(instrucoes[i]);
@@ -63,6 +70,117 @@ class StationGroup {
     return true;
   }
 
+  int perquisaInstrucao(
+      {required List<Instruction> reOrderBuffer,
+      required Instruction instruction}) {
+    for (var i = 0; i < reOrderBuffer.length; i++) {
+      if (reOrderBuffer[i] == instruction) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  // escrita e escrita
+  // escrita e leitura
+  bool dependenciaVerdadeira(
+      Instruction instruction, Instruction novaInstrucao, Station station) {
+    if (instruction.opCode != OpCode.store) {
+      if (instruction.register0 == novaInstrucao.register0) {
+        novaInstrucao.waitRegister = true;
+        station.waitingInstruction.add(novaInstrucao);
+        return true;
+      }
+      if (novaInstrucao.register1 != null) {
+        if (instruction.register0 == novaInstrucao.register1) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+      if (novaInstrucao.register2 != null) {
+        if (instruction.register0 == novaInstrucao.register2) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+    } else {
+      if (instruction.register2 == novaInstrucao.register0) {
+        novaInstrucao.waitRegister = true;
+        station.waitingInstruction.add(novaInstrucao);
+        return true;
+      }
+      if (novaInstrucao.register1 != null) {
+        if (instruction.register2 == novaInstrucao.register1) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+      if (novaInstrucao.register2 != null) {
+        if (instruction.register2 == novaInstrucao.register2) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  bool dependenciaFalsa(
+      Instruction instruction, Instruction novaInstrucao, Station station) {
+    if (instruction.opCode != OpCode.store) {
+      if (instruction.register1 == novaInstrucao.register0) {
+        return true;
+      }
+
+      if (instruction.register0 == novaInstrucao.register0) {
+        novaInstrucao.waitRegister = true;
+        station.waitingInstruction.add(novaInstrucao);
+        return true;
+      }
+      if (novaInstrucao.register1 != null) {
+        if (instruction.register0 == novaInstrucao.register1) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+      if (novaInstrucao.register2 != null) {
+        if (instruction.register0 == novaInstrucao.register2) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+    } else {
+      if (instruction.register2 == novaInstrucao.register0) {
+        novaInstrucao.waitRegister = true;
+        station.waitingInstruction.add(novaInstrucao);
+        return true;
+      }
+      if (novaInstrucao.register1 != null) {
+        if (instruction.register2 == novaInstrucao.register1) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+      if (novaInstrucao.register2 != null) {
+        if (instruction.register2 == novaInstrucao.register2) {
+          novaInstrucao.waitRegister = true;
+          station.waitingInstruction.add(novaInstrucao);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
   // Ocupado = true
   // Não ocupado = false
   bool verifyDependenceRegisters(
@@ -73,93 +191,94 @@ class StationGroup {
   ) {
     var ocupado = false;
     var ocupadoMesmo = false;
-    if(instruction.register0.state == StateRegister.none){
-
-      ocupado = colocaWaitList(registers,instruction.register2,fakeRegisters,instruction);
-      if(ocupado == true ){
+    if (instruction.register0.state == StateRegister.none) {
+      ocupado = colocaWaitList(
+          registers, instruction.register2, fakeRegisters, instruction);
+      if (ocupado == true) {
         ocupadoMesmo = true;
       }
-      colocaWaitList(registers,instruction.register2,fakeRegisters,instruction);
-      if(ocupado == true ){
+      ocupado = colocaWaitList(
+          registers, instruction.register2, fakeRegisters, instruction);
+      if (ocupado == true) {
         ocupadoMesmo = true;
       }
-
-      return ocupadoMesmo;
     }
+    return ocupadoMesmo;
+    // if(instruction.opCode != OpCode.store){
+    //   if(fakeRegisters.containsKey(instruction.register0)){
 
-    if(instruction.opCode != OpCode.store){
-      if(fakeRegisters.containsKey(instruction.register0)){
-        
-      }
-    }
+    //   }
+    // }
 
-    if(){
-      if(instruction.register0.state == StateRegister.reading){
-        return false;
-      }else{
+    // if(){
+    //   if(instruction.register0.state == StateRegister.reading){
+    //     return false;
+    //   }else{
 
-      }
-    }
-    if(fakeRegisters.containsKey(instruction.register1)){
-      return false;
-    }
-    if(fakeRegisters.containsKey(instruction.register2)){
-      return false;
-    }
+    //   }
+    // }
+    // if(fakeRegisters.containsKey(instruction.register1)){
+    //   return false;
+    // }
+    // if(fakeRegisters.containsKey(instruction.register2)){
+    //   return false;
+    // }
 
-    if (instruction.register0.state == StateRegister.recording) {
-      instruction.register0.st!.waitingInstruction.add(instruction);
-    } else if (instruction.register0.state == StateRegister.reading) {
-        if (fakeRegisters.containsKey(instruction.register0) == false) {
-          fakeRegisters[instruction.register0] = instruction.register0.valorRegistrador;
-        }
-      }
+    // if (instruction.register0.state == StateRegister.recording) {
+    //   instruction.register0.st!.waitingInstruction.add(instruction);
+    // } else if (instruction.register0.state == StateRegister.reading) {
+    //     if (fakeRegisters.containsKey(instruction.register0) == false) {
+    //       fakeRegisters[instruction.register0] = instruction.register0.valorRegistrador;
+    //     }
+    //   }
 
-      
-      
-      
-    }
-      
-    if (instruction.register1?.st != null) {}
+    // }
 
-    if (verifyStateRegisters(registers, instruction)) {
-      if (instruction.register0.state != StateRegister.none) {
-        return true;
-      }
+    // if (instruction.register1?.st != null) {}
 
-      if (instruction.register1 != null) {
-        if (instruction.register1?.state != StateRegister.none) {
-          return true;
-        }
-      }
+    // if (verifyStateRegisters(registers, instruction)) {
+    //   if (instruction.register0.state != StateRegister.none) {
+    //     return true;
+    //   }
 
-      if (instruction.register2 != null) {
-        if (instruction.register2?.state != StateRegister.none) {
-          return true;
-        }
-      }
-    }
-    return false;
+    //   if (instruction.register1 != null) {
+    //     if (instruction.register1?.state != StateRegister.none) {
+    //       return true;
+    //     }
+    //   }
+
+    //   if (instruction.register2 != null) {
+    //     if (instruction.register2?.state != StateRegister.none) {
+    //       return true;
+    //     }
+    //   }
+    // }
+    // return false;
   }
 
   // true = pode fazer não
   // false = pode fazer
-  bool colocaWaitList(List<Registrador> registers,
-    Registrador? reg,
-    Map<Registrador, double> fakeRegisters, Instruction instruction){
-    if(reg != null){ // se tem registrador
-        if(reg != StateRegister.none){// se tem alguem utilizando
-          if(reg == StateRegister.recording){// se tiver sendo gravado
-            reg.st!.waitingInstruction.add(instruction);
-            return true;
-          }else{
-            if (fakeRegisters.containsKey(instruction.register0) == false) {
-              fakeRegisters[reg] = instruction.register0.valorRegistrador;
-            }
+  bool colocaWaitList(List<Registrador> registers, Registrador? reg,
+      Map<Registrador, double> fakeRegisters, Instruction instruction) {
+    if (reg != null) {
+      // se tem registrador
+      if (reg != StateRegister.none) {
+        // se tem alguem utilizando
+        if (reg == StateRegister.recording) {
+          // se tiver sendo gravado
+          reg.st!.waitingInstruction.add(instruction);
+          return true;
+        } else {
+          // se tiver lendo
+          if (fakeRegisters.containsKey(instruction.register0) == false) {
+            //se ja tem um fake desse registrador
+            fakeRegisters[reg] = instruction.register0.valorRegistrador;
           }
+          // se não tiver o fake substituir por ele
         }
       }
-      return false;
+    }
+    return false;
   }
 
   // bool verifyStateRegisters(
